8c492f5ea650b0f02312f7c14e5737d5f06b07d8
Mahesh7397 <154659268+Mahesh7397@users.noreply.github.com>
2024-05-30 12:35:17 +0530

    modifide

--- app/Screen/Audiolist.js
+++ app/Screen/Audiolist.js
@@ -4,19 +4,20 @@ import React, { useEffect } from 'react'
 import * as MediaLibrary from 'expo-media-library'
 
 export default function Audiolist() {
-    getPermission=async()=>{
-        const permission=await MediaLibrary.getPermissionsAsync()
-        console.log(permission)
-    }
-
-    useEffect(()=>{
-        getPermission()
-    },[])
+  //  getPermission=async()=>{
+  //      const permission=await MediaLibrary.getPermissionsAsync()
+  //      console.log(permission)
+  //  }
+//
+  //  useEffect(()=>{
+  //      getPermission()
+  //  },[])
+//
   return (
     <View style={styles.container}>
       <Text>Audiolist</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
